30819fcb6d4e4f4cfec380026d826d012de30fcd
Bikram nayak <bikramnayak399@gmail.com>
2022-12-13 17:14:22 +0530

    project

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD0RxibSe-Nrp51v2Ve4ex5z4Qk34K3EqU",
+    authDomain: "capstone-projects-b2e59.firebaseapp.com",
+    projectId: "capstone-projects-b2e59",
+    storageBucket: "capstone-projects-b2e59.appspot.com",
+    messagingSenderId: "1088436159938",
+    appId: "1:1088436159938:web:47b1c297f33916bfd4c25b",
+    measurementId: "G-W4EE26Q4C7"
+};
